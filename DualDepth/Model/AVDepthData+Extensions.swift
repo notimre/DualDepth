11b71f961d349284630dab4380997d86e59bc40f
Imre Draskovits <imre.draskovits@ncl.ac.uk>
2024-07-01 16:24:51 +0100

    Added ARKit, but doesn't work

--- DualDepth/Model/AVDepthData+Extensions.swift
+++ DualDepth/Model/AVDepthData+Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 import AVFoundation
 import UIKit
+import ARKit
 
 //extension AVDepthData {
 //    // convert to AVDepthData (kCVPixelFormatType_DisparityFloat32)
@@ -33,25 +34,54 @@ import UIKit
 //        return normalizedDepthMap.uiImage
 //    }
 //}
+
+
+// working vvvv
+//extension AVDepthData {
+//    // Convert to AVDepthData with kCVPixelFormatType_DepthFloat32
+//    var asDepthFloat32: AVDepthData {
+//        var convertedDepthData: AVDepthData
+//                
+//        // Convert to kCVPixelFormatType_DepthFloat32
+//        if self.depthDataType != kCVPixelFormatType_DepthFloat32 {
+//            convertedDepthData = self.converting(toDepthDataType: kCVPixelFormatType_DepthFloat32)
+//        } else {
+//            convertedDepthData = self
+//        }
+//        
+//        return convertedDepthData
+//    }
+//    
+//    // Convert AVDepthData to UIImage
+//    var asDepthMapImage: UIImage? {
+//        let convertedDepthData = self.asDepthFloat32 // Convert to DepthFloat32
+//        let depthMap = convertedDepthData.depthDataMap.getDepthReading() // Get depth map
+//        return depthMap.uiImage // Convert depth map to UIImage
+//    }
+//}
+// end of working
+
 extension AVDepthData {
-    // Convert to AVDepthData with kCVPixelFormatType_DepthFloat32
     var asDepthFloat32: AVDepthData {
-        var convertedDepthData: AVDepthData
-                
-        // Convert to kCVPixelFormatType_DepthFloat32
-        if self.depthDataType != kCVPixelFormatType_DepthFloat32 {
-            convertedDepthData = self.converting(toDepthDataType: kCVPixelFormatType_DepthFloat32)
-        } else {
-            convertedDepthData = self
-        }
-        
-        return convertedDepthData
+        return self.converting(toDepthDataType: kCVPixelFormatType_DepthFloat32)
     }
     
-    // Convert AVDepthData to UIImage
     var asDepthMapImage: UIImage? {
-        let convertedDepthData = self.asDepthFloat32 // Convert to DepthFloat32
-        let depthMap = convertedDepthData.depthDataMap.getDepthReading() // Get depth map
-        return depthMap.uiImage // Convert depth map to UIImage
+        // Create a temporary ARSCNView
+        let sceneView = ARSCNView(frame: CGRect(x: 0, y: 0, width: 1, height: 1))
+        
+        // Example cgPt and disparity values; these should be derived from your specific use case
+        let cgPt = CGPoint(x: 100, y: 100) // Example point
+        let disparity: Float = 0.1 // Example disparity
+        
+        // Convert depth data to DepthFloat32
+        let convertedDepthData = self.asDepthFloat32
+        let depthMap = convertedDepthData.depthDataMap
+        
+        // Call getDepthReading on the depth map
+        let modifiedDepthMap = depthMap.getDepthReading(sceneView: sceneView, cgPt: cgPt, disparity: disparity)
+        
+        // Convert the resulting depth map to UIImage
+        return modifiedDepthMap.uiImage
     }
 }
